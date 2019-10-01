31d24e990b5ec3eae3f765803ace281ec2bc9158
Antoine Deshaies <antoine958.ad@gmail.com>
2019-10-01 12:10:32 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
 ###application.bat - 201x - Antoine Deshaies###
-echo "version 1.1"
-echo "Copyright company - 201x"
-echo "Build : 10:00"
+version ="2"
+build_hour ="11H00"
+
+echo "Build${build_hour}"
+echo "version${version}"
